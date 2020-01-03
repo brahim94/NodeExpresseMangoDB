c6fd2328e73fd3361ff10f43be362a309dc55b8a
HimDev <brahim-ayad@hotmail.com>
2020-01-03 23:04:54 +0100

    GET REQUEST FOR ONE PRODUCT ONLY FIXING SHCEMA

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    userId: { type: String, required: false},
+    userId: { type: String, required: true},
     price: { type: Number, required: true },
     inStock: { type: Boolean, required: true},
 
